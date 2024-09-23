9bd56cdfe2d7439ca45b7e466f88aa95d79b64e1
Maty <matyquintas@gmail.com>
2024-09-23 16:12:40 -0300

    Clase 01

---
+++ 02. JavaScript/Clases/Clase 01/index.js
@@ -0,0 +1,93 @@
+// SEGUNDA PARTE JS
+/*
+SEGUNDA PARTE JS
+*/
+
+// VARIABLES CON LET 
+
+
+// TIPO DE DATOS
+
+let curso="javaScript"
+let numero=10
+let boolean_true=true
+let boolean_false=false
+let nombre='JUAN'
+
+let CURSO='Desarrollo web'
+let anio=2024
+
+let dia,fecha,hora,lugar,celular,precio,verdadero;
+
+dia='sabado';
+fecha='20-7';
+hora='15:01';
+lugar='Coder';
+celular='Samsung';
+precio=100.5;
+verdadero=true;
+
+dia=true
+
+// CONSTANTE 
+let nombre1='enzo';
+let apellido='agú';
+let espacio =' '
+let nombreCompleto= nombre1 + espacio + apellido
+// console.log(nombreCompleto);
+
+const anio1=2024;
+const numeroPi=3.14;
+
+const n1=200;
+const n2=2;
+
+let resultado = n1 / n2;
+// console.log(resultado);
+
+// anio1=''
+
+// console.log(anio,dia,verdadero,fecha);
+// console.log('Cadena texto');
+// console.log(true);
+// console.log(anio1);
+
+let n4='1'
+let n5='3'
+
+let resultado2=n4 + n5
+// console.log(resultado2);
+
+// let pregunta=prompt('Ingrese su nombre')
+// let pregunta2=prompt('Ingrese su apellido')
+
+// alert('Gracias' + ' ' + pregunta + ' ' + pregunta2)
+
+// let precio2=parseInt(prompt(`Ingrese un precio`))
+// console.log(typeof precio2)
+// alert(`Precio ingresado ${precio2}`)
+
+const anioActual=2024;
+let fechaNacimiento=Number(prompt('Inngrese su edad'));
+console.log(typeof fechaNacimiento);
+
+let fechaFinal=anioActual - fechaNacimiento
+console.log(fechaFinal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
