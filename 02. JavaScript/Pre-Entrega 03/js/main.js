ae6f19d2ea40f12a7e0e153cd16730ba0da8f00c
Maty <matyquintas@gmail.com>
2024-09-23 22:56:54 -0300

    Pre Entrega 03

--- 02. JavaScript/Pre-Entrega 03/js/main.js
+++ 02. JavaScript/Pre-Entrega 03/js/main.js
@@ -1,35 +1,54 @@
 function main() 
 {
-    let verProductos = JSON.parse(jsonProducts);
+    const verProductos = JSON.parse(jsonProducts);
 
     let divProductos = document.getElementById('productos');
 
-    for (let i = 0; i < verProductos.length; i++) {
-        // Inserto los productos en el elemento id Productos para cargarlos de manera dinamica independientemente del json
-        if (verProductos[i].Discount != 0) {
+    let verCarrito = JSON.parse(localStorage.getItem('verCarrito')) || [];
+
+    verProductos.forEach((producto, i) => {
+        if (producto.Discount != 0) {
             divProductos.innerHTML += (`
                 <div class="producto">
-                    <img src="${verProductos[i].Img}">
-                    <h3 class="titulo">${verProductos[i].Title}</h3>
-                    <p class="descripcion">${verProductos[i].Excerpt}</p>
-                    <span class="sale">$${(verProductos[i].Price * (1 - (verProductos[i].Discount / 100))).toFixed(2)} 
-                        <span class="pAnterior">${verProductos[i].Price}</span>
+                    <img src="${producto.Img}">
+                    <h3 class="titulo">${producto.Title}</h3>
+                    <p class="descripcion">${producto.Excerpt}</p>
+                    <span class="sale">$${(producto.Price * (1 - (producto.Discount / 100))).toFixed(2)} 
+                        <span class="pAnterior">$${producto.Price}</span>
                     </span>
-                    <p class="sku">SKU: ${verProductos[i].SKU}</p>
+                    <span class="sku">SKU: ${producto.SKU}</span>
+                    <button id="btn0${i}" class="addToCart" type="button">Añadir al carrito</button>
                 </div>
             `);
         } else {
             divProductos.innerHTML += (`
                 <div class="producto">
-                    <img src="${verProductos[i].Img}">
-                    <h3 class="titulo">${verProductos[i].Title}</h3>
-                    <p class="descripcion">${verProductos[i].Excerpt}</p>
-                    <span class="precio">$${verProductos[i].Price}</span>
-                    <p class="sku">SKU: ${verProductos[i].SKU}</p>
+                    <img src="${producto.Img}">
+                    <h3 class="titulo">${producto.Title}</h3>
+                    <p class="descripcion">${producto.Excerpt}</p>
+                    <span class="precio">$${producto.Price}</span>
+                    <span class="sku">SKU: ${producto.SKU}</span>
+                    <button id="btn${i}" class="addToCart" type="button">Añadir al carrito</button>
                 </div>
             `);
         }
-    }
+    });
+
+    const botonesCart = document.getElementsByClassName("addToCart");
+
+    Array.from(botonesCart).forEach((button, i) => {
+        button.addEventListener("click", function () {
+            const productoSeleccionado = verProductos[i];
+
+            verCarrito.push(productoSeleccionado);
+
+            localStorage.setItem('verCarrito', JSON.stringify(verCarrito));
+
+            console.log("Producto añadido:", productoSeleccionado);
+            console.log("Carrito actual:", verCarrito);
+        });
+    });
+    console.log(Array.from(botonesCart))
 }
 
 main();
